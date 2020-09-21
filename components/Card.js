c6318a227266e32001ae249ebc929d7ea8b88aaa
cawauchi6204 <cawauchi6204@gmail.com>
2020-09-22 01:01:39 +0900

    build

--- components/Card.js
+++ components/Card.js
@@ -1,25 +1,25 @@
 import React from 'react'
-import Link from 'next/link';
-import styled from 'styled-components'
 
+const Card = (props) => {
+    return (
 
-const Card = styled.card({
-    border: 'black 1px solid'
-})
-export default Card = (props) => {
-    <article>
-        <div className={postThumbnail}>
+        <article>
+            <p>こんにちは</p>
+            {/* <div>
             <Link to={props.link}>
-                <img src="" alt="" />
+            <img src="" alt="" />
             </Link>
-        </div>
-        <div className={postBody}>
+            </div>
+            <div className={postBody}>
             <div className={postMeta}>
             </div>
             <div className={postTitle}>
-                <div className={postCategory}>
-                </div>
+            <div className={postCategory}>
             </div>
-        </div>
-    </article>
+            </div>
+        </div> */}
+        </article>
+    )
 }
+
+export default Card
