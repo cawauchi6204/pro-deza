cf24810024138f4c14dd4c572503c3afa3228f41
cawauchi6204 <cawauchi6204@gmail.com>
2020-10-01 20:36:11 +0900

    Cardデザインひとまず完成

--- components/molecules/Card.jsx
+++ components/molecules/Card.jsx
@@ -5,22 +5,28 @@ import CardImage from '../atoms/CardImage'
 const Card = ({ blog }) => {
     return (
         <>
-            <div key={blog.id} className="vertical-card bg-gray w-3/12 flex flex-col justify-between" >
+            <div key={blog.id} className="vertical-card bg-gray w-3/12 flex flex-col justify-evenly">
                 <div>
                     <div>
                         <Link href="/blogs/[id]" as={`blogs/${blog.id}`}>
-                            <CardImage image={blog.eyeCatch.url} className="w-full" height={'auto'} />
+                            <a>
+                                <CardImage image={blog.eyeCatch.url} className="w-full cursor-pointer" height={"50%"} />
+                            </a>
                         </Link>
                     </div>
-                    <Link href="/blogs/[id]" as={`blogs/${blog.id}`}>
-                        <h2 className="text-2xl leading-loose">{blog.title}</h2>
-                    </Link>
-                    <p>{blog.publishDate}</p>
-                    {blog.tags.map(tag => (
-                        <React.Fragment key={tag.id}>
-                            <span>{tag.name}</span>
-                        </React.Fragment>
-                    ))}
+                    <div className="h-32 flex flex-col justify-evenly text-center">
+                        <Link href="/blogs/[id]" as={`blogs/${blog.id}`}>
+                            <div className="cursor-pointer">
+                                <p className="text-base leading-loose">{blog.title}</p>
+                                {blog.tags.map(tag => (
+                                    <React.Fragment key={tag.id}>
+                                        <span className="text-xs">{tag.name}</span>
+                                    </React.Fragment>
+                                ))}
+                                <p className="text-xs">{blog.publishDate}</p>
+                            </div>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </>
